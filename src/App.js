38d7615937a88d00e3612b27aba0fada64b4d76b
Aeiswarja Biswas <aeiswarjabiswas98@gmail.com>
2024-05-14 16:17:19 +0530

    created counter using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+
+  const handleChange = (event) => {
+    // console.log(event);
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Counter </h1>
+      <h2>{count}</h2>
+      <button onClick={increment} disabled={count === 5}>
+        Increment
+      </button>
+      <button onClick={decrement} disabled={count === -10}>
+        Decrement
+      </button>
+      <button onClick={reset} disabled={count === 0}>
+        Reset
+      </button>
+
+      <h2>{inputValue}</h2>
+      <input
+        type="text"
+        placeholder="enter your name"
+        value={inputValue}
+        onChange={handleChange}
+      />
     </div>
   );
 }
-
 export default App;
